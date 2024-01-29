0ff35b8dd51e362c57fdb848925145e0249d007b
gloz0315 <qudtn0315@naver.com>
2024-01-29 20:23:57 +0900

    로그인 및 회원가입에 관한 기능 생성 및 리팩토링

--- src/main/java/com/miniproject/todoproject/controller/UserController.java
+++ src/main/java/com/miniproject/todoproject/controller/UserController.java
@@ -1,24 +1,16 @@
 package com.miniproject.todoproject.controller;
 
-import com.miniproject.todoproject.dto.ResponseDto;
+import com.miniproject.todoproject.dto.LoginResponseDto;
 import com.miniproject.todoproject.dto.SignupRequestDto;
 import com.miniproject.todoproject.dto.UserInfoDto;
-import com.miniproject.todoproject.entity.UserRoleEnum;
 import com.miniproject.todoproject.security.UserDetailsImpl;
 import com.miniproject.todoproject.service.UserService;
-import jakarta.validation.Valid;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
@@ -26,42 +18,20 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
     private final UserService userService;
-    
-    @GetMapping("/user/login-page")
-    public String loginPage() {
-        return "login";
-    }
-    
-    @GetMapping("/user/signup")
-    public String signupPage() {
-        return "signup";
-    }
 
     // 회원 가입하기
     @PostMapping("/user/signup")
-    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
-        // Validation 예외처리
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        if(!fieldErrors.isEmpty()) {
-            for(FieldError fieldError : fieldErrors) {
-                log.error(fieldError.getField() + "필드 : " + fieldError.getDefaultMessage());
-            }
-            return "redirect:/api/user/signup";
-        }
-        
-        userService.signup(requestDto);
-        
-        return "redirect:/api/user/login-page";
-    }
-
-    @GetMapping("/user-info")
     @ResponseBody
-    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        String username= userDetails.getUsername();
-        UserRoleEnum role = userDetails.getUser().getRole();
-        boolean isAdmin = (role == UserRoleEnum.ADMIN);
-
-        return new UserInfoDto(username, isAdmin);
+    public LoginResponseDto signup(@RequestBody SignupRequestDto request) {
+        return userService.signup(request);
     }
+
+    // 로그인 하기
+    @PostMapping("/user/login")
+    @ResponseBody
+    public LoginResponseDto login(@RequestBody SignupRequestDto request, HttpServletResponse response) {
+        return userService.login(response,request);
+    }
+    
 
 }
