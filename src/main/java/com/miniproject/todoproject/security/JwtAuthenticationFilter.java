d8d089a31995f195461fca2dd540d28131a73ccf
gloz0315 <qudtn0315@naver.com>
2024-01-29 16:45:12 +0900

    인증과 관련된 코드 적용(강의 내용에서 조금 수정하여 적용)

---
+++ src/main/java/com/miniproject/todoproject/security/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.miniproject.todoproject.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miniproject.todoproject.dto.LoginRequestDto;
+import com.miniproject.todoproject.entity.UserRoleEnum;
+import com.miniproject.todoproject.jwtUtil.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+        setFilterProcessesUrl("/api/user/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getUsername(),
+                            requestDto.getPassword(),
+                            null
+                    )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    // 성공하면 HttpServletRepsonse의 헤더에 토큰을 보내서 넣어주기 -> 헤더에는 잘 간 것을 확인
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+        String username = ((UserDetailsImpl) authResult.getPrincipal()).getUsername();
+        UserRoleEnum role = ((UserDetailsImpl) authResult.getPrincipal()).getUser().getRole();
+
+        String token = jwtUtil.createToken(username, role);
+        log.info("생성된 토큰 : " + token);                          // 토큰 체크 -> 잘 갔는지?
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        response.setStatus(401);
+    }
+}
