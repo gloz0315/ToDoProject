fb56ee64b199dad013e921c29443548f7db48056
gloz0315 <qudtn0315@naver.com>
2024-01-31 23:21:29 +0900

    Swagger 적용

--- src/main/java/com/miniproject/todoproject/dto/usersDto/UsersToDoResponseDto.java
+++ src/main/java/com/miniproject/todoproject/dto/usersDto/UsersToDoResponseDto.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 import com.miniproject.todoproject.dto.tododto.ToDoResponseDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UsersToDoResponseDto {
+	@Schema(description = "유저 이름", example = "robbie")
 	private String username;
+	@Schema(description = "전체 할일 카드 목록들")
 	private List<ToDoResponseDto> todoList = new ArrayList<>();
 
 	public UsersToDoResponseDto(String username, List<ToDoResponseDto> responseList) {
