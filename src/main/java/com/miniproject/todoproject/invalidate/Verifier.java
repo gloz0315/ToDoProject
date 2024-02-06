d2e274ca15f3c2abd20d928a5e502647b731f68c
gloz0315 <qudtn0315@naver.com>
2024-02-06 10:52:59 +0900

    UserRepository 인터페이스 default 메서드를 통한 처리

--- src/main/java/com/miniproject/todoproject/invalidate/Verifier.java
+++ src/main/java/com/miniproject/todoproject/invalidate/Verifier.java
@@ -2,7 +2,6 @@ package com.miniproject.todoproject.invalidate;
 
 import java.util.List;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.miniproject.todoproject.entity.Comment;
@@ -21,46 +20,10 @@ public class Verifier {
 	private final UserRepository userRepository;
 	private final TodoRepository todoRepository;
 	private final CommentRepository commentRepository;
-	private PasswordEncoder passwordEncoder;
-
-	// 유저에 대한 검증 메서드들
-	public User verifyUser(String username) {
-		return userRepository.findByUsername(username).orElseThrow(
-			() -> new IllegalArgumentException(Message.NOT_EXIST_USER)
-		);
-	}
-
-	public void comparePassword(String repositoryPassword, String password) {
-		if (!passwordEncoder.matches(password, repositoryPassword)) {
-			throw new IllegalArgumentException(Message.NOT_MATCH_PASSWORD);
-		}
-	}
-
-	public void invalidateUsername(String username) {
-		if (!Invalidate.userLengthValidate(username)) {
-			throw new IllegalArgumentException(Message.INVALIDATE_USERNAME);
-		}
-	}
-
-	public void invalidatePassword(String password) {
-		if (!Invalidate.passwordLengthValidate(password)) {
-			throw new IllegalArgumentException(Message.INVALIDATE_PASSWORD);
-		}
-	}
-
-	public void duplicateSName(String username) {
-		if (Invalidate.duplicateUserName(userRepository.findByUsername(username))) {
-			throw new IllegalArgumentException(Message.DUPLICATE_USERNAME);
-		}
-	}
 
 	// Todo에 대한 검증 메서드
 	public List<User> findAllUsers() {
 		return userRepository.findAll();
-	}
-
-	public List<Comment> findAllComments(Todo todo) {
-		return commentRepository.findByTodo(todo);
 	}
 
 	public void checkCompareUser(User todoUser, User user) {
@@ -76,9 +39,7 @@ public class Verifier {
 	}
 
 	public User findUser(String username) {
-		return userRepository.findByUsername(username).orElseThrow(
-			() -> new IllegalArgumentException(Message.NOT_EXIST_USER)
-		);
+		return userRepository.findByUserNameOrElseThrow(username);
 	}
 
 	// Comment에 대한 검증
--- src/main/java/com/miniproject/todoproject/repository/UserRepository.java
+++ src/main/java/com/miniproject/todoproject/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package com.miniproject.todoproject.repository;
 
-import com.miniproject.todoproject.entity.User;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import com.miniproject.todoproject.entity.User;
+import com.miniproject.todoproject.message.Message;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);
+
+	default User findByUserNameOrElseThrow(String username) {
+		return findByUsername(username).orElseThrow(
+			() -> new IllegalArgumentException(Message.NOT_EXIST_USER)
+		);
+	}
 }
