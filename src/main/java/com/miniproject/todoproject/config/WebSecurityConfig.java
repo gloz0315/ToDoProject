59f9998d019c52b18ab7cdb3906d138883f4705f
gloz0315 <qudtn0315@naver.com>
2024-02-22 15:31:07 +0900

    :recycle: refactor code to WebSecurityConfig

--- src/main/java/com/miniproject/todoproject/config/WebSecurityConfig.java
+++ src/main/java/com/miniproject/todoproject/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.miniproject.todoproject.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.miniproject.todoproject.jwtUtil.JwtUtil;
 import com.miniproject.todoproject.security.JwtAuthorizationFilter;
 import com.miniproject.todoproject.security.UserDetailsServiceImpl;
@@ -25,6 +27,7 @@ public class WebSecurityConfig {
 
 	private final JwtUtil jwtUtil;
 	private final UserDetailsServiceImpl userDetailsService;
+	private final ObjectMapper objectMapper;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -38,7 +41,7 @@ public class WebSecurityConfig {
 
 	@Bean
 	public JwtAuthorizationFilter jwtAuthorizationFilter() {
-		return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
+		return new JwtAuthorizationFilter(jwtUtil, userDetailsService, objectMapper);
 	}
 
 	@Bean
@@ -46,10 +49,19 @@ public class WebSecurityConfig {
 		// CSRF 설정
 		http.csrf((csrf) -> csrf.disable());
 
+		// 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
 		http.sessionManagement((sessionManagement) ->
 			sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		);
 
+		http.authorizeHttpRequests((authorizeHttpRequests) ->
+			authorizeHttpRequests
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+				.requestMatchers("/api/users/**").permitAll() // '/api/users/'로 시작하는 요청 모두 접근 허가
+				.anyRequest().authenticated() // 그 외 모든 요청 인증처리
+		);
+
+		// 필터 관리
 		http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
